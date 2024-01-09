4395b3f456628bda3ff74d19a02fe6ed8122708d
hardik9977 <35445211+hardik9977@users.noreply.github.com>
2024-01-09 22:48:56 +0530

    for test git workflow

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 
 const PORT = 3000;
 const start = async () => {
+    console.log("Test");
     if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
         throw new Error("JWT_KEY or MONGO_URI not found");
     }
